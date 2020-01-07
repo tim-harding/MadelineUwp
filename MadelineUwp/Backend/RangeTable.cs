2e38fd5cd9543a4c4a270a8f23e5046636f43433
timmythetiny <timharding@me.com>
2020-01-06 22:41:59 -0800

    Changed RangeTable implementation.

--- MadelineUwp/Backend/RangeTable.cs
+++ MadelineUwp/Backend/RangeTable.cs
@@ -4,13 +4,15 @@ namespace Madeline.Backend
 {
     internal class RangeTable<T>
     {
-        private Table<Range> indices = new Table<Range>();
+        private int next;
+        private List<int> ids = new List<int>();
+        private List<int> starts = new List<int>();
         private List<T> values = new List<T>();
 
         public void Extend(int count, T template)
         {
-            var range = new Range(values.Count, values.Count + count);
-            indices.Insert(range);
+            ids.Add(next++);
+            starts.Add(values.Count);
             for (int i = 0; i < count; i++)
             {
                 values.Add(template);
@@ -19,14 +21,14 @@ namespace Madeline.Backend
 
         public void Insert(IEnumerable<T> multiple)
         {
-            int start = values.Count;
+            ids.Add(next++);
+            starts.Add(values.Count);
             values.AddRange(multiple);
-            indices.Insert(new Range(start, values.Count));
         }
 
         public void Update(int id, int offset, T value)
         {
-            if (indices.TryGet(id, out Range range) && offset < range.Count())
+            if (TryGetRange(id, out Range range) && offset < range.Count)
             {
                 values[range.start + offset] = value;
             }
@@ -34,42 +36,54 @@ namespace Madeline.Backend
 
         public void Delete(int id)
         {
-            if (indices.TryGet(id, out Range range))
+            int index = ids.BinarySearch(id);
+            if (index > -1)
             {
-                int count = range.Count();
-                values.RemoveRange(range.start, count);
-                indices.Delete(id);
-                foreach (TableRow<Range> row in indices)
+                int start = starts[index];
+                int end = index < starts.Count - 1 ? starts[index + 1] : values.Count;
+                int count = end - start;
+                for (int i = index; i < starts.Count; i++)
                 {
-                    if (row.id > id)
-                    {
-                        Range update = row.value;
-                        update.start -= count;
-                        update.end -= count;
-                        indices.Update(row.id, update);
-                    }
+                    starts[i] -= count;
                 }
+                ids.RemoveAt(index);
+                starts.RemoveAt(index);
+                values.RemoveRange(start, count);
             }
         }
 
         public bool TryGet(int id, out ListSlice<T> slice)
         {
-            if (indices.TryGet(id, out Range range))
+            if (TryGetRange(id, out Range range))
             {
                 slice = new ListSlice<T>(values, range);
                 return true;
             }
-            slice = new ListSlice<T>(values, new Range(0, 0));
+            slice = default;
             return false;
         }
 
         public ListSlice<T> Get(int id)
         {
-            if (indices.TryGet(id, out Range range))
+            if (TryGetRange(id, out Range range))
             {
                 return new ListSlice<T>(values, range);
             }
             return new ListSlice<T>(values, new Range(0, 0));
+        }
+
+        private bool TryGetRange(int id, out Range range)
+        {
+            int index = ids.BinarySearch(id);
+            if (index > -1)
+            {
+                int start = starts[index];
+                int end = index < starts.Count - 1 ? starts[index + 1] : starts.Count;
+                range = new Range(start, end);
+                return true;
+            }
+            range = default;
+            return false;
         }
     }
 }
